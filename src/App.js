1d5c45fd9962f328c3bd4946ba83ead9349218db
Katya <13gordienkok@gmail.com>
2020-10-17 00:08:06 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main.js";
+// import Features from "./components/Features/Features";
+import Footer from "./components/Footer/Footer";
+// import Calendar from "./components/Calendar/Calendar";
+// import Details from "./components/Details/Details";
+import FetchData from "./service/FetchData";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./style.css";
+// const app = React.createElement("div", { classNameName: "App" }, "Привет миру!!!");
+class App extends React.Component {
+  fetchData = new FetchData();
+  state = {
+    rocket: "Falcon 1",
+    rocketFeatures: null,
+    rockets: [],
+  };
+
+  componentDidMount() {
+    // console.log(this.fetchData.getLaunches().then((data) => console.log(data)));
+    this.updateRocket();
+  }
+
+  updateRocket() {
+    // console.log(this.state);
+    this.fetchData
+      .getRocket()
+      .then((data) => {
+        this.setState({ rockets: data.map((item) => item.name) });
+        return data;
+      })
+      .then((data) => data.find((item) => item.name === this.state.rocket))
+      .then((rocketFeatures) => this.setState({ rocketFeatures }));
+    // console.log(this.state);
+  }
+
+  chengeRocket = (rocket) => {
+    this.setState(
+      {
+        rocket,
+      },
+      this.updateRocket
+    );
+  };
+
+  render() {
+    console.log(this.state);
+    return (
+      <React.Fragment>
+        <Header rockets={this.state.rockets} chengeRocket={this.chengeRocket} />
+        <Main rocket={this.state.rocket} />
+        <Footer />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
